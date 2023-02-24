f249ced5d4d05b80f14cb6603fec0051e31b7269
Icaro Cristolfe <cristolfeicaro@gmail.com>
2023-02-24 00:40:54 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,112 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+
+import {
+  Container,
+  FraseContainer,
+  ContainerItems,
+  InputContainer,
+  SortButton,
+  EditContainer,
+  SortContainer,
+  ButtonsContainer,
+  H1
+} from './style'
 
 function App() {
+  const [frases, setFrases] = useState([]);
+  const [fraseAtual, setFraseAtual] = useState('');
+  const [proximoId, setProximoId] = useState(1);
+  const [fraseEditada, setFraseEditada] = useState('');
+  const [fraseEditadaId, setFraseEditadaId] = useState(null);
+  const [sortContainer, setSortContainer] = useState(false)
+  const [indice, setIndice] = useState()
+
+  function adicionarFrase() {
+    const novaFrase = { id: proximoId, texto: fraseAtual };
+    setFrases([...frases, novaFrase]);
+    setFraseAtual('');
+    setProximoId(proximoId + 1);
+  }
+
+  function removerFrase(id) {
+    const novasFrases = frases.filter(frase => frase.id !== id);
+    setFrases(novasFrases);
+  }
+
+  function editarFrase(id) {
+    const fraseParaEditar = frases.find(frase => frase.id === id);
+    setFraseEditada(fraseParaEditar.texto);
+    setFraseEditadaId(id);
+  }
+
+  function salvarFraseEditada() {
+    const novasFrases = frases.map(frase => {
+      if (frase.id === fraseEditadaId) {
+        return { id: frase.id, texto: fraseEditada };
+      }
+      return frase;
+    });
+    setFrases(novasFrases);
+    setFraseEditada('');
+    setFraseEditadaId(null);
+  }
+
+  function sortearFrase() {
+    if (frases.length === 0) {
+      alert('Adicione pelo menos uma frase antes de sortear!');
+      return;
+    }
+  
+    const indice = Math.floor(Math.random() * frases.length);
+    setSortContainer(true);
+    setIndice(indice);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+    <Container>
+      <H1>Plataforma de Sorteios</H1>
+      <InputContainer>
+      <input value={fraseAtual} onChange={e => setFraseAtual(e.target.value)}/>
+      <button onClick={adicionarFrase}>Adicionar Tema</button>
+      </InputContainer>
+      
+      <ContainerItems>
+
+        {frases.map(frase => (
+          <FraseContainer key={frase.id}>
+            {frase.texto}
+            <div>
+            <ButtonsContainer onClick={() => removerFrase(frase.id)}>Remover</ButtonsContainer>
+            <ButtonsContainer onClick={() => editarFrase(frase.id)}>Editar</ButtonsContainer>
+            </div>
+          </FraseContainer>
+        ))}
+     
+     </ContainerItems>
+      <SortButton onClick={sortearFrase}>Sortear Tema</SortButton>
+
+      {fraseEditadaId && (
+        <EditContainer>
+          <input value={fraseEditada} onChange={e => setFraseEditada(e.target.value)} />
+          <button onClick={salvarFraseEditada}>Salvar edição</button>
+        </EditContainer>
+      )}
+
+      {
+        sortContainer && (
+        <SortContainer>
+          <h2>{ frases[indice].texto }</h2>
+          <div>
+          <ButtonsContainer onClick={sortearFrase}>Sortear</ButtonsContainer>
+          <ButtonsContainer onClick={e => setSortContainer(false)}>Ok</ButtonsContainer>
+          </div>
+        </SortContainer>
+      )}
+    
+      
+    </Container>
+    
   );
 }
 
---
+++ src/style.js
@@ -0,0 +1,190 @@
+import styled from 'styled-components'
+
+
+export const Container = styled.div`
+    background: rgb(0,48,255);
+    background: radial-gradient(circle, rgba(0,48,255,1) 0%, rgba(32,42,74,1) 100%);
+    height: 100vh;
+    color: #000 ;
+    padding: 50px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+`
+export const InputContainer = styled.div `
+    width: 80%;
+    height: 60px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 20px;
+
+    input {
+        width: 300px;
+        height:  40px;
+        border: none;
+        border-radius: 10px;
+        padding-left: 15px;
+    }
+
+    button {
+    width: 150px;
+    height: 40px;
+    border: none;
+    border-radius: 10px;
+    cursor: pointer;
+    font-size: 16px;
+    background: #202A4A;
+    color: #E3E3E3;
+
+    &:hover{
+        opacity: 0.8;
+    }
+
+    &:active{
+        opacity: 0.6;
+    }
+    }
+`
+
+export const FraseContainer = styled.div`
+    background-color: #191970;
+    width: 90%;
+    height: 100px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    flex-direction: column;
+    padding: 0 25px;
+    margin-bottom: 10px;
+    border-radius: 10px;
+    color: #E3E3E3;
+    background: rgba(255, 255, 255, 0.2);
+    border-radius: 10px;
+    box-shadow: 0 8px 32px 0 rgba(31, 38, 135, 0.37);
+    backdrop-filter: blur(10px);
+    -webkit-backdrop-filter: blur(10px);
+    padding:  10px;
+    font-size: 18px;
+    font-weight: 600;
+  
+
+
+    div {
+        display: flex;
+        gap: 20px;
+    }
+`
+
+export const ContainerItems = styled.div `
+    margin: 50px 0 ;
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr 1fr;
+
+`
+
+export const SortButton = styled.button `
+    background: #202A4A;
+    color: #e3e3e3;
+    width: 29%;
+    height: 40px;
+    border: none;
+    border-radius: 10px;
+    cursor: pointer;
+
+    &:hover {
+    opacity: 0.8
+        }
+
+    &:active {
+    opacity: 0.6;
+        }
+
+`
+
+export const EditContainer = styled.div`
+    margin-top: 50px;
+    display: flex;
+    flex-direction: column;
+
+    input{
+        width: 29%;
+        height:  40px;
+        border: none;
+        border-radius: 10px;
+        padding-left: 15px;
+        margin-bottom: 20px;
+    }
+
+    button {
+    background: #202A4A;
+    color: #e3e3e3;
+    width: 29%;
+    height: 40px;
+    border: none;
+    border-radius: 10px;
+    cursor: pointer;
+
+    &:hover {
+    opacity: 0.8
+        }
+
+    &:active {
+    opacity: 0.6;
+        }
+    }
+`
+
+export const SortContainer = styled.div `
+    background-color: #191970;
+    width: 29%;
+    height: 200px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    padding: 0 25px;
+    margin-bottom: 10px;
+    border-radius: 10px;
+    color: #E3E3E3;
+    background: rgba(255, 255, 255, 0.2);
+    border-radius: 10px;
+    box-shadow: 0 8px 32px 0 rgba(31, 38, 135, 0.37);
+    backdrop-filter: blur(10px);
+    -webkit-backdrop-filter: blur(10px);
+    padding:  10px;
+    font-size: 18px;
+    font-weight: 600;
+    margin-top: 50px;
+
+    div {
+        display: flex;
+
+        gap: 10px;
+    }
+`
+
+export const ButtonsContainer = styled.button `
+        background: #202A4A;
+        color: #e3e3e3;
+        width: 100px;
+        height: 40px;
+        border: none;
+        border-radius: 10px;
+        cursor: pointer;
+
+        &:hover {
+            opacity: 0.8
+        }
+
+        &:active {
+            opacity: 0.6;
+        }
+    
+`
+
+export const H1 = styled.h1`
+    color: #e3e3e3;
+    margin-bottom: 50px;
+`
